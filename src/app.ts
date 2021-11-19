d0744c711d0ac4c6a235236c60f5a09438b6b9b2
cokakoala <adriangonz457@gmail.com>
2021-11-19 16:33:22 -0500

    removed unused code

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,14 @@
 import express from "express";
-// import path from "path";
-// import { fileURLToPath } from "url";
 import logger from "./logger/index";
 import auth from "./api/oauth/auth/index";
-import revoke from "./api/oauth/revoke/index";
 import { getActiveClients } from "./api/bot/index";
 
 const app = express();
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(express.static(__dirname + "/pages"));
 app.post("/auth", auth);
-// app.get("/clients", getActiveClients);
+app.get("/clients", getActiveClients);
 
 app.listen(process.env.PORT);
 logger.info(`Running on port ${process.env.PORT}`);
